9bd4f3c9c75ba462760e5a7b0c14134f12d7922d
Ryland Sherman <rylandsherman1@gmail.com>
2023-12-05 01:38:11 -0700

    contact page update

--- src/components/ContactPage.js
+++ src/components/ContactPage.js
@@ -1,21 +1,141 @@
-import React from "react";
+import React, { useState } from "react";
 import "../index.css";
 
 const ContactPage = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    date: "",
+    email: "",
+    phoneNumber: "",
+    description: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form submitted:", formData);
+    setFormData({
+      firstName: "",
+      lastName: "",
+      date: "",
+      email: "",
+      phoneNumber: "",
+      description: "",
+    });
+  };
+
   return (
     <div className="container">
       <div className="contact-content">
         <h1>Contact Page</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-          voluptatem, voluptatum, quibusdam, quas voluptas quae doloremque
-          voluptatibus quod quia accusantium aperiam. Quisquam voluptatem,
-          voluptatum, quibusdam, quas voluptas quae doloremque voluptatibus quod
-          quia accusantium aperiam.
-        </p>
+        <p>Fill out the form below to contact us:</p>
+
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="firstName" className="label">
+              First Name:
+            </label>
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              value={formData.firstName}
+              onChange={handleChange}
+              className="input"
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="lastName" className="label">
+              Last Name:
+            </label>
+            <input
+              type="text"
+              id="lastName"
+              name="lastName"
+              value={formData.lastName}
+              onChange={handleChange}
+              className="input"
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="date" className="label">
+              Date:
+            </label>
+            <input
+              type="date"
+              id="date"
+              name="date"
+              value={formData.date}
+              onChange={handleChange}
+              className="input"
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="email" className="label">
+              Email:
+            </label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="input"
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="phoneNumber" className="label">
+              Phone Number:
+            </label>
+            <input
+              type="tel"
+              id="phoneNumber"
+              name="phoneNumber"
+              value={formData.phoneNumber}
+              onChange={handleChange}
+              className="input"
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="description" className="label">
+              Description:
+            </label>
+            <textarea
+              id="description"
+              name="description"
+              value={formData.description}
+              onChange={handleChange}
+              className="input"
+              required
+            />
+          </div>
+
+          <button type="submit" className="submit-button">
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   );
 };
 
 export default ContactPage;
+
+
